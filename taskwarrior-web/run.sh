208e8327a66ab789daca88f0f9929237bf125d24
Bharti <casualuminary@users.noreply.github.com>
2026-07-15 18:17:30 +0200

    trying with build args

--- taskwarrior-web/run.sh
+++ taskwarrior-web/run.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bash
 
 export TASKDATA=/config/.task
 
 mkdir -p "$TASKDATA"
 
 cd /app/taskwarrior-web || exit 1
-echo "PWD=$(pwd)"
-ls -la
 
 exec bundle exec task-web \
-  -o 0.0.0.0 \
-  -p 5678
+    -o 0.0.0.0 \
+    -p 5678
 
-echo "task-web exited: $?"
-sleep 300
+echo "PID=$$"
+ps
